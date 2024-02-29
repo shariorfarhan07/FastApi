db75622c44c829b5da8f74a398d33806dab3e5fa
shariorfarhan07 <shariorfarhan77@gmail.com>
2024-02-29 21:42:20 +0600

    add

---
+++ practice jwt/model.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.ext.declarative import  declarative_base
+from  sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine,Column,Integer,String,Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
